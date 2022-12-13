51195904eed1490229d235459ff6ac61a09128e3
a.elwardi <a.elwardi98gmail.Com>
2022-12-13 20:56:00 +0100

    Etat initial

---
+++ src/main/java/ma/elwardi/tp1_abderrazzak_elwardi/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package ma.elwardi.tp1_abderrazzak_elwardi;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
